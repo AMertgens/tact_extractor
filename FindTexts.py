7546446089aedfbd09ced5f47c77f4b4f562bc62
Andreas Mertgens <a.mertgens@googlemail.com>
2024-08-20 15:10:00 +0200

    update

--- FindTexts.py
+++ FindTexts.py
@@ -5,8 +5,9 @@ import csv
 import re
 import json
 
-# Specify the file path
+# Specify the file path of the CSV file (parsed list of quotes)
 file_path = 'nostri-parsed_data.csv'
+# Load the dictionary
 with open('saved_dictionary_iterate.pkl', 'rb') as f:
     para_dict = pickle.load(f)
 
@@ -15,24 +16,31 @@ with open(file_path, 'r') as file:
     csv_reader = csv.reader(file)
     textfound= 0
     for row in csv_reader:
+        # for each row in the CSV file, check if the snippet is found in the full text
         textfoundlocal = 0
         snippet =  normalized_string = re.sub(r'\s+', ' ', row[3])
         print("Snippet: ", snippet)
         idString = [row[0], row[1], row[2]]
         for key, value in para_dict.items():
+            # Normalize the full text by removing extra whitespace
             fulltext =  normalized_string = re.sub(r'\s+', ' ', value[0])
             if snippet in fulltext :
+                # If the snippet is found in the full text, increment the counter
                 textfound += 1
                 textfoundlocal += 1
+                # Add the identifier to the list of identifiers for the snippet
                 value[1].append(idString)
                 #print(textfound,textfoundlocal)
                 """ print("Found",snippet, key)
                 time.sleep(1) """
             if textfoundlocal > 1:
+                # If the snippet is found in more than one full text, print a warning
                 print("Warning",snippet, key)
                 time.sleep(1)
                 break
     print(textfound)
+
+# Save the dictionary
     with open('saved_dictionary_iterate2.pkl', 'wb') as f:
         pickle.dump(para_dict, f)     
     json_file_path = 'data3.json'
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tact_extractor
+This repository contains scripts to assist retrieval of data from a TACT database.
+
+1. **parseRawData.py**
+   - **Input:** `data.txt` (renamed from `.tdb`)
+   - Parses full texts from raw TACT database file and saves them into a JSON file and Python dictionary.
+
+2. parseLists
+- **Manual Step in TACT Instance
+-
+- **Input:** *.txt (renamed from '.lst')
